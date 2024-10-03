93710830666b442d7d98c7ce9914e0ba5204263a
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-03 21:09:12 +0100

    feat: Create ServicesTemplate and design and style it

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,7 @@ import { Button } from "@/components/Button";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
 import SEO from "@/components/SEO";
+import { ServicesTemplate } from "@/components/ServicesTemplate";
 import React from "react";
 
 export default function Home() {
@@ -19,6 +20,7 @@ export default function Home() {
             </main> */}
             <Header />
             <Hero />
+            <ServicesTemplate />
 
         </div>
     );
