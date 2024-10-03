0ecca84829aa2adb29db9c68728240f82a8a27df
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-03 20:25:51 +0100

    Adjust icons layout

--- src/components/Companies.tsx
+++ src/components/Companies.tsx
@@ -4,11 +4,11 @@ import Image from "next/image";
 
 export const Companies = () => {
     return (
-        <section>
-            <div>
+        <section className="flex w-full justify-center">
+            <div className="flex w-[75%] justify-between">
                 {Logos.map((logo, index) => (
                     <div key={index} className="flex items-center justify-center">
-                        <Image src={logo.logo} alt={logo.name} className="h-20 w-20" />
+                        <Image src={logo.logo} alt={logo.name} width={100} height={100} className="lg:h-32 h-20 w-20 lg:w-32 opacity-75" />
                     </div>
                 ))}
             </div>
--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,31 +1,36 @@
 import Image from "next/image";
 import { Button } from "./Button";
+import { Companies } from "./Companies";
 
 export const Hero = () => {
-    return ( 
-        <div style={{ backgroundImage: "url('/assets/images/background.svg')" }} className="flex min-h-screen w-full flex-col items-center justify-center pt-12 lg:flex-row ">
-            <div className="flex flex-col items-center text-left space-y-7 w-full">
-                <h1 className="text-7xl font-semibold text-[#8B5CF6] ">
-                    Insightful trends <br />
-                    <span className="text-black">to discover real <br /> opportunities</span>
-                </h1>
-                <div className="text-xl font-medium text-[#374151] px-28">
-                    <p>
-                        Step into the new era of strategic decision making. Horizon empowers  businesses in the marketing space with unparalleled data analytics.
-                    </p>
+    return (
+        <div style={{ backgroundImage: "url('/assets/images/background.svg')" }}>
+            <div className="flex min-h-screen w-full flex-col items-center justify-center pt-12 lg:flex-row ">
+                <div className="flex w-full flex-col items-center space-y-7 text-left">
+                    <h1 className="text-7xl font-semibold text-[#8B5CF6] ">
+                        Insightful trends <br />
+                        <span className="text-black">
+                            to discover real <br /> opportunities
+                        </span>
+                    </h1>
+                    <div className="px-28 text-xl font-medium text-[#374151]">
+                        <p>Step into the new era of strategic decision making. Horizon empowers businesses in the marketing space with unparalleled data analytics.</p>
+                    </div>
+                    <div className="space-x-7">
+                        <Button backgroundColor={"bg-[#FDFDFD]"} color={"text-black"}>
+                            Discover more
+                        </Button>
+                        <Button backgroundColor={"bg-gradient-to-r from-[#9B72F7] to-[#8B5CF6]"} color={"text-white"}>
+                            Get early access
+                        </Button>
+                    </div>
                 </div>
-                <div className="space-x-7">
-                    <Button backgroundColor={"bg-[#FDFDFD]"} color={"text-black"}>
-                        Discover more
-                    </Button>
-                    <Button backgroundColor={"bg-gradient-to-r from-[#9B72F7] to-[#8B5CF6]"} color={"text-white"}>
-                        Get early access
-                    </Button>
+                <div className="w-full ">
+                    <Image src="/assets/images/hero-img.svg" alt={"Image showing transacrions"} width={1000} height={1000} />
                 </div>
             </div>
-            <div className="w-full ">
-                <Image src="/assets/images/hero-img.svg" alt={"Image showing transacrions"} width={1000} height={1000} />
-            </div>
+
+            <Companies />
         </div>
     );
 };
