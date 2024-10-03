a904fecde8f3ed66034462dcdae86de924e00de7
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-03 18:39:20 +0100

    Update font styles and add Header component

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,7 +1,23 @@
 import React from "react";
 
 export const Header = () => {
-    return <div>Header</div>;
+    return (
+        <div>
+            <div>
+                <h1 className="text-2xl font-bold">Horizon</h1>
+            </div>
+            <div>
+                <nav>
+                    <ul className="text-lg font-medium text-gray-500">
+                        <li>Features</li>
+                        <li>Use Case</li>
+                        <li>Integration</li>
+                        <li>Pricing</li>
+                        <li>Blog</li>
+                    </ul>
+                </nav>
+            </div>
+            <div></div>
+        </div>
+    );
 };
-
-
