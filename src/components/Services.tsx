9ea3a327df435dbe33b14fb30935d3b8dd71f7c1
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-03 22:54:37 +0100

    feat: Add the second serviceTemplate component with updated date

--- src/components/Services.tsx
+++ src/components/Services.tsx
@@ -1,10 +1,23 @@
+import React from "react";
 import { ServicesTemplate } from "./ServicesTemplate";
 
 export const Services = () => {
     return (
-        <ServicesTemplate
-            image={"assets/images/serviceone.svg"}
-            title={"Uncover latest trends in real life"}
-            text={"Stay ahead of the curve with our Dynamic Trend Analysis feature Horizon continuously monitors and analyzes trends in real time, providing you with instant insights into latest shifts in consumer behavior and interests."} useHeader={true} reverse={false}        />
+        <>
+            <ServicesTemplate
+                image={"assets/images/serviceone.svg"}
+                title={"Uncover latest trends in real life"}
+                text={"Stay ahead of the curve with our Dynamic Trend Analysis feature Horizon continuously monitors and analyzes trends in real time, providing you with instant insights into latest shifts in consumer behavior and interests."}
+                useHeader={true}
+                reverse={false}
+            />
+            <ServicesTemplate
+                image={"assets/images/servicetwo.svg"}
+                title={"Tailor-made insights, just for you"}
+                text={"Empower your decision-makers with personalized insights. Our Customizable Insights Dashboards allow you to curate the data that matters most to your business."}
+                useHeader={false}
+                reverse={true}
+            />
+        </>
     );
 };
