081aa37e33613577d2dc1f477c0bc130190e250e
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-03 22:31:21 +0100

    Refactor ServicesTemplate to use props for image, title, and text

--- src/components/ServicesTemplate.tsx
+++ src/components/ServicesTemplate.tsx
@@ -46,14 +46,14 @@ export const ServicesTemplate = ({image, title, text}: ServicesTemplateProps) =>
             </header>
             <div className="flex min-h-screen w-full  flex-col items-center justify-center gap-10 lg:flex-row">
                 <div className="flex w-full justify-center">
-                    <Image src={"assets/images/serviceone.svg"} alt={""} width={1000} className="h-full w-[70%] lg:h-[85%] lg:w-[85%]" height={1000} />
+                    <Image src={image} alt={""} width={1000} className="h-full w-[70%] lg:h-[85%] lg:w-[85%]" height={1000} />
                 </div>
                 <section className="flex w-full flex-col items-center justify-center">
                     <div className="flex w-full flex-col items-center space-y-6 px-5 sm:px-24">
                         <h1 className="text-5xl font-semibold">
-                            <span className=" text-[#8B5CF6]">Uncover</span> latest trends in real life
+                            <span className=" text-[#8B5CF6]">{title}</span>
                         </h1>
-                        <p className="text-base font-light">Stay ahead of the curve with our Dynamic Trend Analysis feature Horizon continuously monitors and analyzes trends in real time, providing you with instant insights into latest shifts in consumer behavior and interests. </p>
+                        <p className="text-base font-light">{text} </p>
                     </div>
                     <div></div>
                 </section>
