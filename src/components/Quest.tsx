a295ad97a07e3adb99f19190331312964c14caf8
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-04 16:08:43 +0100

    feat: create an array containing contents of the quest grid

--- src/components/Quest.tsx
+++ src/components/Quest.tsx
@@ -3,13 +3,23 @@ import { Badge } from "./Badge";
 export const Quest = () => {
     return (
         <div className="flex min-h-screen w-full items-center justify-center bg-[#F9FAFB]">
-            <div className="flex  text-center flex-col justify-center">
-                <div className="w-full flex justify-center">
+            <div className="flex  flex-col justify-center text-center">
+                <div className="flex w-full justify-center">
                     <Badge>Features</Badge>
                 </div>
                 <h1 className="text-6xl font-semibold">
-                    Essentially our quest<span className="text-[#9CA3AF] block">here at horizon</span>
+                    Essentially our quest<span className="block text-[#9CA3AF]">here at horizon</span>
                 </h1>
+            </div>
+            <div>
+                <div className="grid w-[80%] grid-cols-3 grid-rows-2">
+                    {/* <div><h2>Trend Detection</h2></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div>
+                    <div></div> */}
+                </div>
             </div>
         </div>
     );
---
+++ src/utilities/quest.ts
@@ -0,0 +1,8 @@
+export const QuestContent = [
+    { src: "", title: "Trend Detection", description: "Horizon’s AI-powered Trend Detection feature identifies emerging trends in your industry, ensuring you’re always ahead of the curve." },
+    { src: "", title: "Daily updates", description: "Identify strengths, capitalize on weaknesses, and chart a course for unparalleled success." },
+    { src: "", title: "Market Coverage", description: "Identify strengths, capitalize on weaknesses, and chart a course for unparalleled success." },
+    { src: "", title: "Start small", description: "Identify strengths, capitalize on weaknesses, and chart a course for unparalleled success." },
+    { src: "", title: "Other markets", description: "Identify strengths, capitalize on weaknesses, and chart a course for unparalleled success." },
+    { src: "", title: "Get the indicator", description: "Identify strengths, capitalize on weaknesses, and chart a course for unparalleled success." },
+];
