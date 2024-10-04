9ec7eb7bdbc5f8711f00e63ab094821605d90361
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-04 15:54:48 +0100

    refactor: Add a header to quest layout

--- src/components/Quest.tsx
+++ src/components/Quest.tsx
@@ -3,10 +3,12 @@ import { Badge } from "./Badge";
 export const Quest = () => {
     return (
         <div className="flex min-h-screen w-full items-center justify-center bg-[#F9FAFB]">
-            <div className="flex w-[40%] flex-col justify-center">
-                <Badge>Features</Badge>
+            <div className="flex  text-center flex-col justify-center">
+                <div className="w-full flex justify-center">
+                    <Badge>Features</Badge>
+                </div>
                 <h1 className="text-6xl font-semibold">
-                    Essentially our quest<span className="text-[#9CA3AF]">here at horizon</span>
+                    Essentially our quest<span className="text-[#9CA3AF] block">here at horizon</span>
                 </h1>
             </div>
         </div>
