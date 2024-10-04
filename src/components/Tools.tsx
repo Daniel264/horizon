c593000380a7ee195972180f0009c6a8db6bc0b4
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-04 22:44:15 +0100

    reduce the sizes of the divs in tools component

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -4,7 +4,7 @@ import Image from "next/image";
 
 export const Tools = () => {
     return (
-        <div className="flex min-h-screen w-full items-center justify-center">
+        <div className="flex min-h-screen w-full items-center justify-center px-5 py-10">
             <div className="flex w-full flex-col items-center">
                 <div className="flex w-[78%] flex-col items-center space-y-5">
                     <div>
@@ -18,74 +18,74 @@ export const Tools = () => {
                     </div>
                 </div>
             </div>
-            <div className="flex h-full w-full flex-row gap-10">
-                <div className="flex flex-row space-y-10 lg:flex-col">
-                    <div className="flex h-[225px] w-[225px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
+            <div className="flex h-full w-full flex-row gap-3">
+                <div className="flex flex-row space-y-3 lg:flex-col">
+                    <div className="flex h-[200px] w-[200px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
                         <div className="h-full w-full">
                             <Image src={"/assets/logo/notion.svg"} alt={"Notion logo"} width={40} height={40} />
                         </div>
                         <p className="text-xl font-semibold">Notion</p>
-                        <p>Plan your life</p>
+                        <p className="font-medium text-[#4B5563]">Plan your life</p>
                     </div>
-                    <div className="flex h-[225px] w-[225px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
+                    <div className="flex h-[200px] w-[200px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
                         <div className="h-full w-full">
                             <Image src={"/assets/logo/loom.svg"} alt={"Notion logo"} width={40} height={40} />
                         </div>{" "}
                         <p className="text-xl font-semibold">Loom</p>
-                        <p>Plan your life</p>
+                        <p className="font-medium text-[#4B5563]">Plan your life</p>
                     </div>
-                    <div className="flex  h-[225px] w-[225px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
+                    <div className="flex  h-[200px] w-[200px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
                         <div className="h-full w-full">
                             <Image src={"/assets/logo/things.svg"} alt={"Notion logo"} width={40} height={40} />
                         </div>
                         <p className="text-xl font-semibold">Things</p>
-                        <p>Plan your life</p>
+                        <p className="font-medium text-[#4B5563]">Plan your life</p>
                     </div>
                 </div>
-                <div className="flex flex-row space-y-10 lg:flex-col">
-                    <div className="flex h-[225px] w-[225px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
+                <div className="flex flex-row space-y-3 lg:flex-col">
+                    <div className="flex h-[200px] w-[200px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
                         <div className="h-full w-full">
                             <Image src={""} alt={""} width={40} height={40} />
                         </div>
                         <p className="text-xl font-semibold">Confluence</p>
-                        <p>Plan your life</p>
+                        <p className="font-medium text-[#4B5563]">Plan your life</p>
                     </div>
-                    <div className="flex h-[225px] w-[225px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
+                    <div className="flex h-[200px] w-[200px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
                         <div className="h-full w-full">
                             <Image src={"/assets/logo/mailchimp.svg"} alt={"Notion logo"} width={40} height={40} />
                         </div>
                         <p className="text-xl font-semibold">Mail Chimp</p>
-                        <p>Plan your life</p>
+                        <p className="font-medium text-[#4B5563]">Plan your life</p>
                     </div>
-                    <div className="flex h-[225px] w-[225px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
+                    <div className="flex h-[200px] w-[200px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
                         <div className="h-full w-full">
                             <Image src={"/assets/logo/github.svg"} alt={"Notion logo"} width={40} height={40} />
                         </div>
                         <p className="text-xl font-semibold">GitHub</p>
-                        <p>Plan your life</p>
+                        <p className="font-medium text-[#4B5563]">Plan your life</p>
                     </div>
                 </div>
-                <div className="flex flex-row space-y-10 lg:flex-col">
-                    <div className="flex h-[225px] w-[225px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
+                <div className="flex flex-row space-y-3 lg:flex-col">
+                    <div className="flex h-[200px] w-[200px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
                         <div className="h-full w-full">
                             <Image src={"/assets/logo/codepen.svg"} alt={"Notion logo"} width={40} height={40} className="" />
                         </div>
                         <p className="text-xl font-semibold">Codepen</p>
-                        <p>Plan your life</p>
+                        <p className="font-medium text-[#4B5563]">Plan your life</p>
                     </div>
-                    <div className="flex h-[225px] w-[225px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
+                    <div className="flex h-[200px] w-[200px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
                         <div className="h-full w-full">
                             <Image src={"/assets/logo/intercom.svg"} alt={"Notion logo"} width={40} height={40} />
                         </div>
                         <p className="text-xl font-semibold">Intercom</p>
-                        <p>Plan your life</p>
+                        <p className="font-medium text-[#4B5563]">Plan your life</p>
                     </div>
-                    <div className="flex h-[225px] w-[225px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
+                    <div className="flex h-[200px] w-[200px] flex-col  rounded-badge border-2 border-black border-opacity-10 p-5 text-left">
                         <div className="h-full w-full">
                             <Image src={"/assets/logo/slack.svg"} alt={"Notion logo"} width={40} height={40} />
                         </div>
                         <p className="text-xl font-semibold">Slack</p>
-                        <p>Plan your life</p>
+                        <p className="font-medium text-[#4B5563]">Plan your life</p>
                     </div>
                 </div>
             </div>
