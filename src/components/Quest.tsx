3b18f89c5be5e25c19dee53e6d120c17681e0fe4
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-04 17:11:57 +0100

    Refactor: add border's to each grid element and increase height

--- src/components/Quest.tsx
+++ src/components/Quest.tsx
@@ -3,7 +3,7 @@ import { Badge } from "./Badge";
 
 export const Quest = () => {
     return (
-        <div className="flex min-h-screen w-full flex-col items-center justify-center bg-[#F9FAFB]">
+        <div className="flex min-h-screen w-full flex-col items-center justify-center space-y-10 bg-[#F9FAFB]">
             <div className="flex  flex-col justify-center text-center">
                 <div className="flex w-full justify-center">
                     <Badge>Features</Badge>
@@ -14,9 +14,9 @@ export const Quest = () => {
             </div>
             <div className="flex items-center justify-center">
                 <div className="flex w-[80%] justify-center">
-                    <div className="grid w-full grid-cols-3 grid-rows-2">
+                    <div className="grid w-full grid-cols-3 grid-rows-2 gap-9 ">
                         {QuestContent.map((content, index) => (
-                            <div key={index}>
+                            <div key={index} className="h-[300px] border-2 border-gray-500 border-opacity-10 bg-[#ffffff]">
                                 <div className="flex justify-center">
                                     <img src={content.src} alt="icon" />
                                 </div>
