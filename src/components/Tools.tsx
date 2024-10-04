927fda7359bccf7c2d961744f123156965adbb52
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-04 22:17:43 +0100

    Refactor Tools component layout and add logos for integrations

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -18,22 +18,58 @@ export const Tools = () => {
                     </div>
                 </div>
             </div>
-            <div className="w-full">
+            <div className="flex h-full w-full flex-row">
                 <div className="flex flex-row lg:flex-col">
-                    <div className="flex h-full w-full flex-col items-center justify-center rounded-badge border-black border-opacity-10 shadow outline-2 outline-black">
+                    <div className="flex h-full w-full flex-col items-center justify-center rounded-badge border-2 border-black border-opacity-10">
                         <Image src={"/assets/logo/notion.svg"} alt={"Notion logo"} width={100} height={100} />
                         <p className="text-xl font-medium">Notion</p>
                         <p>Plan your life</p>
                     </div>
-                    <div></div>
-                    <div></div>
+                    <div className="flex h-full w-full flex-col items-center justify-center rounded-badge border-2 border-black border-opacity-10">
+                        <Image src={"/assets/logo/loom.svg"} alt={"Notion logo"} width={100} height={100} />
+                        <p className="text-xl font-medium">Loom</p>
+                        <p>Plan your life</p>
+                    </div>
+                    <div className="flex h-full w-full flex-col items-center justify-center rounded-badge border-2 border-black border-opacity-10">
+                        <Image src={"/assets/logo/things.svg"} alt={"Notion logo"} width={100} height={100} />
+                        <p className="text-xl font-medium">Things</p>
+                        <p>Plan your life</p>
+                    </div>
                 </div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
-                <div></div>
+                <div className="flex flex-row lg:flex-col">
+                    <div className="flex h-full w-full flex-col items-center justify-center rounded-badge border-2 border-black border-opacity-10">
+                        <Image src={"/assets/logo/notion.svg"} alt={"Notion logo"} width={100} height={100} />
+                        <p className="text-xl font-medium">Notion</p>
+                        <p>Plan your life</p>
+                    </div>
+                    <div className="flex h-full w-full flex-col items-center justify-center rounded-badge border-2 border-black border-opacity-10">
+                        <Image src={"/assets/logo/mailchimp.svg"} alt={"Notion logo"} width={100} height={100} />
+                        <p className="text-xl font-medium">Loom</p>
+                        <p>Plan your life</p>
+                    </div>
+                    <div className="flex h-full w-full flex-col items-center justify-center rounded-badge border-2 border-black border-opacity-10">
+                        <Image src={"/assets/logo/github.svg"} alt={"Notion logo"} width={100} height={100} />
+                        <p className="text-xl font-medium">Things</p>
+                        <p>Plan your life</p>
+                    </div>
+                </div>
+                <div className="flex flex-row lg:flex-col">
+                    <div className="flex h-full w-full flex-col items-center justify-center rounded-badge border-2 border-black border-opacity-10">
+                        <Image src={"/assets/logo/codepen.svg"} alt={"Notion logo"} width={100} height={100} />
+                        <p className="text-xl font-medium">Notion</p>
+                        <p>Plan your life</p>
+                    </div>
+                    <div className="flex h-full w-full flex-col items-center justify-center rounded-badge border-2 border-black border-opacity-10">
+                        <Image src={"/assets/logo/intercom.svg"} alt={"Notion logo"} width={100} height={100} />
+                        <p className="text-xl font-medium">Loom</p>
+                        <p>Plan your life</p>
+                    </div>
+                    <div className="flex h-full w-full flex-col items-center justify-center rounded-badge border-2 border-black border-opacity-10">
+                        <Image src={"/assets/logo/slack.svg"} alt={"Notion logo"} width={100} height={100} />
+                        <p className="text-xl font-medium">Things</p>
+                        <p>Plan your life</p>
+                    </div>
+                </div>
             </div>
         </div>
     );
