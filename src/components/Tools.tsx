6796f101be41c6fce924a9ff504f19b8077f1bfd
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-04 22:11:08 +0100

    refactor: align the first half of the tools section to the center

--- src/components/Tools.tsx
+++ src/components/Tools.tsx
@@ -5,16 +5,17 @@ import Image from "next/image";
 export const Tools = () => {
     return (
         <div className="flex min-h-screen w-full items-center justify-center">
-            <div className="w-full">
-                <div>
-                    <Badge>Integrations</Badge>
-                </div>
-                <div>
-                    <h1 className="text-6xl font-semibold">
-                        Smart <span className="text-[#8B5CF6]">integrations</span>
-                        at your fingertips
-                    </h1>
-                    <p className="text-[#374151]">Our open API and robust integration framework ensures a smooth connection with the tools you rely on daily, creating a unified ecosystem for unparalleled efficiency.</p>
+            <div className="flex w-full flex-col items-center">
+                <div className="flex w-[78%] flex-col items-center space-y-5">
+                    <div>
+                        <Badge>Integrations</Badge>
+                    </div>
+                    <div className="space-y-5">
+                        <h1 className="text-6xl font-semibold">
+                            Smart <span className="text-[#8B5CF6]">integrations</span> at your fingertips
+                        </h1>
+                        <p className="font-medium text-[#374151]">Our open API and robust integration framework ensures a smooth connection with the tools you rely on daily, creating a unified ecosystem for unparalleled efficiency.</p>
+                    </div>
                 </div>
             </div>
             <div className="w-full">
