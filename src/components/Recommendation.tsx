a89a9cf38b4d30437276679afa47ec255c5e190c
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-04 12:23:47 +0100

    feat: Add Recommendation component

---
+++ src/components/Recommendation.tsx
@@ -0,0 +1,18 @@
+import { Badge } from "./Badge";
+
+export const Recommendation = () => {
+    return (
+        <section className="flex min-h-screen w-full items-center justify-center">
+            <header>
+                <Badge>Customer Voices</Badge>
+                <h1 className="text-6xl font-semibold ">Early adopters are saying</h1>
+            </header>
+            <div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </section>
+    );
+};
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import { Button } from "@/components/Button";
 import { Header } from "@/components/Header";
 import { Hero } from "@/components/Hero";
+import { Recommendation } from "@/components/Recommendation";
 import SEO from "@/components/SEO";
 import { Services } from "@/components/Services";
 import { ServicesTemplate } from "@/components/ServicesTemplate";
@@ -11,19 +12,21 @@ export default function Home() {
         <div className="">
             <SEO title="Home" />
 
-            {/* <main className="flex min-h-screen flex-col items-center p-5"> */}
+             <main className="flex min-h-screen flex-col items-center"> 
             {/* <div className="m-auto w-full max-w-8xl"> */}
             {/* 
                 <div className="max-w-8xl m-auto">
                 
                     <h3 className="my-5 text-5xl font-semibold">Home</h3>
                 </div>
-            </main> */}
+            */}
             <Header />
             <Hero />
             {/* <ServicesTemplate /> */}
             <Services />
+            <Recommendation />
 
+            </main> 
         </div>
     );
 }
