2b2578ef7f0c6f5e9f0857e8453c849bceeb10c2
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-04 17:07:39 +0100

    align the grid to the center of the page

--- src/components/Quest.tsx
+++ src/components/Quest.tsx
@@ -3,7 +3,7 @@ import { Badge } from "./Badge";
 
 export const Quest = () => {
     return (
-        <div className="flex min-h-screen w-full items-center justify-center bg-[#F9FAFB]">
+        <div className="flex min-h-screen w-full flex-col items-center justify-center bg-[#F9FAFB]">
             <div className="flex  flex-col justify-center text-center">
                 <div className="flex w-full justify-center">
                     <Badge>Features</Badge>
@@ -12,20 +12,22 @@ export const Quest = () => {
                     Essentially our quest<span className="block text-[#9CA3AF]">here at horizon</span>
                 </h1>
             </div>
-            <div>
-                <div className="grid w-[80%] grid-cols-3 grid-rows-2">
-                    {QuestContent.map((content, index) => (
-                        <div key={index}>
-                            <div className="flex justify-center">
-                                <img src={content.src} alt="icon" />
+            <div className="flex items-center justify-center">
+                <div className="flex w-[80%] justify-center">
+                    <div className="grid w-full grid-cols-3 grid-rows-2">
+                        {QuestContent.map((content, index) => (
+                            <div key={index}>
+                                <div className="flex justify-center">
+                                    <img src={content.src} alt="icon" />
+                                </div>
+                                <div className="text-center">
+                                    <h1 className="text-3xl font-semibold">{content.title}</h1>
+                                    <p className="text-[#9CA3AF]">{content.description}</p>
+                                </div>
                             </div>
-                            <div className="text-center">
-                                <h1 className="text-3xl font-semibold">{content.title}</h1>
-                                <p className="text-[#9CA3AF]">{content.description}</p>
-                            </div>
-                        </div>
-                    ))}
-                    <div></div> 
+                        ))}
+                        <div></div>
+                    </div>
                 </div>
             </div>
         </div>
