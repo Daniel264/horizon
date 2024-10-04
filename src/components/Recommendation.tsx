7929f13003069e667beaa887a35356cd87dfc907
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-04 12:34:39 +0100

    feat: add contents including button component to recommendation section

--- src/components/Recommendation.tsx
+++ src/components/Recommendation.tsx
@@ -1,4 +1,5 @@
 import { Badge } from "./Badge";
+import { Button } from "./Button";
 
 export const Recommendation = () => {
     return (
@@ -7,10 +8,29 @@ export const Recommendation = () => {
                 <Badge>Customer Voices</Badge>
                 <h1 className="text-6xl font-semibold ">Early adopters are saying</h1>
             </header>
-            <div>
-                <div></div>
-                <div></div>
-                <div></div>
+            <div className="grid grid-cols-1 lg:grid-cols-2">
+                <div>
+                    <div>
+                        <p className="text-xl font-light">We integrated Horizon into our business operations, and the results have been outstanding. The Smart Alert System ensures we&apos;re always informed about critical market developments, giving us a competitive edge.</p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <p className="text-xl font-light">We integrated Horizon into our business operations, and the results have been outstanding. The Smart Alert System ensures we&apos;re always informed about critical market developments, giving us a competitive edge.</p>
+                    </div>
+                </div>
+                <div>
+                    <div>
+                        <h1 className="text-5xl font-semibold">
+                            Benchmark <span className="text-[#8B5CF6]">against</span> the best companies
+                        </h1>
+
+                        <p className="text-xl font-light">Elevate your competitive edge with our Competitor Benchmarking feature. Horizon benchmarks your performance against industry leaders, providing actionable insights to outperform your competition.</p>
+                        <Button backgroundColor={"bg-[#9B72F7]"} color={"text-white"}>
+                            Get early access
+                        </Button>
+                    </div>
+                </div>
                 <div></div>
             </div>
         </section>
