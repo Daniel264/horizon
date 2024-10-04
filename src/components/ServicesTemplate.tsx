cef0096d2b1f5fa9697889ba0ecd28bde047d050
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-04 11:25:32 +0100

    refactor: Update section icon size and text styling in ServicesTemplate

--- src/components/ServicesTemplate.tsx
+++ src/components/ServicesTemplate.tsx
@@ -60,19 +60,19 @@ export const ServicesTemplate = ({ image, title, text, useHeader, reverse }: Ser
                         <div className="flex flex-col md:flex-row">
                             <div className="space-y-4">
                                 <div className="w-fit rounded-2xl bg-[#EDE9FE] px-2 py-2">
-                                    <Image src="/assets/images/section-icon.svg" alt="" width={100} height={100} className="h-12 w-12" />
+                                    <Image src="/assets/images/section-icon.svg" alt="" width={100} height={100} className="h-8 w-8" />
                                 </div>
-                                <h1 className="text-3xl font-semibold">See the Whole Picture</h1>
-                                <p className="font-base text-base">For anyone to start building their real estate portfolio, no matter the size of your wallet.</p>
+                                <h1 className="text-2xl font-semibold">See the Whole Picture</h1>
+                                <p className="font-base text-sm">For anyone to start building their real estate portfolio, no matter the size of your wallet.</p>
                                 <p>Learn more</p>
                             </div>
                             <div className="space-y-4">
                                 <div className="w-fit rounded-2xl bg-[#DBEAFE] px-2 py-2">
-                                    <Image src="/assets/images/section-icon.svg" alt="" width={100} height={100} className="h-12 w-12" />
+                                    <Image src="/assets/images/section-icon.svg" alt="" width={100} height={100} className="h-8 w-8" />
                                 </div>
-                                <h1 className="text-3xl font-semibold">See the Whole Picture</h1>
+                                <h1 className="text-2xl font-semibold">See the Whole Picture</h1>
 
-                                <p className="">For anyone to start building their real estate portfolio, no matter the size of your wallet.</p>
+                                <p className="text-sm">For anyone to start building their real estate portfolio, no matter the size of your wallet.</p>
                                 <p>Learn more</p>
                             </div>
                         </div>
