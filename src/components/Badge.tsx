291771a6e4351702cbf81a64490a85d789f2a2eb
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-04 12:16:25 +0100

    feat: Add a small circle beside the badge text

--- src/components/Badge.tsx
+++ src/components/Badge.tsx
@@ -3,5 +3,10 @@ interface BadgeProps {
 }
 
 export const Badge = ({ children }: BadgeProps) => {
-    return <div className="flex w-fit items-center justify-center rounded bg-[#EDE9FE] px-3 py-2 text-xs uppercase  text-[#8B5CF6]">{children}</div>;
+    return (
+        <div className="flex w-fit items-center justify-center rounded bg-[#EDE9FE] px-3 py-2 text-xs uppercase gap-2 text-[#8B5CF6]">
+            <div className="h-2 w-2 rounded-full bg-[#8B5CFC] "></div>
+            {children}
+        </div>
+    );
 };
