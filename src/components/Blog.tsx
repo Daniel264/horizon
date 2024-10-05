8c4a89640d32c411778e97ad74636a64825f5ed5
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-05 19:37:13 +0100

    feat: add Blog component and initial blog content

---
+++ src/components/Blog.tsx
@@ -0,0 +1,16 @@
+import Image from "next/image";
+import { Badge } from "./Badge";
+
+export const Blog = () => {
+    <div className="min-h-screen w-full">
+        <div>
+            <Badge>Blog</Badge>
+            <h1 className="text-6xl">Read, learn, improve</h1>
+        </div>
+        <div>
+            
+        </div>
+    </div>;
+};
+
+// <Image src="/assets/images/blog-one.svg" alt="blog image" width={500} height={500} />
---
+++ src/utilities/blog.ts
@@ -0,0 +1,5 @@
+export const BlogContent = [
+    { src: "/assets/images/blog-one.svg", alt: "blog image", title: "What is CAC and how do i calculate it?", badge: "articles" },
+    { src: "/assets/images/blog-two.svg", alt: "blog image", title: "How you can use recurring revenue financing for faster growth without dilution", badge: "case studies" },
+    { src: "/assets/images/blog-three.svg", alt: "blog image", title: "Announcing Slang.ai's $20M in funding", badge: "funding" },
+];
