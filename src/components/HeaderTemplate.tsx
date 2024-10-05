12a39ed42ec097883580595c7bdb9e9a75d88580
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-05 20:20:28 +0100

    feat: add conditional rendering to HeaderTemplate for dynamic content display

--- src/components/HeaderTemplate.tsx
+++ src/components/HeaderTemplate.tsx
@@ -1,23 +1,40 @@
 import React from "react";
 import { Button } from "./Button";
 
-export const HeaderTemplate = () => {
+interface HeaderTemplateProps {
+    useHeader: boolean;
+}
+
+export const HeaderTemplate = ({ useHeader }: HeaderTemplateProps) => {
     return (
-        <div className="flex h-fit w-full items-center justify-between px-3 md:px-12 py-5">
+        <div className="flex h-fit w-full items-center justify-between px-3 py-5 md:px-12">
             <div>
                 <h1 className="text-2xl font-bold">Horizon</h1>
             </div>
-            <div className="hidden lg:flex">
-                <nav>
-                    <ul className="flex  space-x-5 font-medium text-gray-700 hover:cursor-pointer sm:text-base lg:text-lg">
-                        <li>Features</li>
-                        <li>Use Case</li>
-                        <li>Integration</li>
-                        <li>Pricing</li>
-                        <li>Blog</li>
-                    </ul>
-                </nav>
-            </div>
+            {useHeader ? (
+                <div className="hidden lg:flex">
+                    <nav>
+                        <ul className="flex  space-x-5 font-medium text-gray-700 hover:cursor-pointer sm:text-base lg:text-lg">
+                            <li>Features</li>
+                            <li>Use Case</li>
+                            <li>Integration</li>
+                            <li>Pricing</li>
+                            <li>Blog</li>
+                        </ul>
+                    </nav>
+                </div>
+            ) : (
+                <div className="hidden lg:flex">
+                    Design by{" "}
+                    <a className="text-gray-500 hover:text-black" href="https://twitter.com/AndreaMontini">
+                        Andrea Montini
+                    </a>{" "}
+                    | Development by{" "}
+                    <a className="text-gray-500 hover:text-black" href="https://x.com/DanielOlatinsu">
+                        Daniel Olatinsu
+                    </a>
+                </div>
+            )}
             <div>
                 <Button backgroundColor={"bg-gradient-to-r from-[#9B72F7] to-[#8B5CF6]"} color={"text-white"}>
                     Get this Template
