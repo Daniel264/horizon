6ada53a600faf0986cc0ba3e1a9c894f183f0832
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-05 19:51:25 +0100

    refactor: enhance Blog component layout with improved styling and responsiveness

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -4,21 +4,23 @@ import { BlogContent } from "@/utilities/blog";
 
 export const Blog = () => {
     return (
-        <div className="min-h-screen w-full">
-            <div>
-                <Badge>Blog</Badge>
-                <h1 className="text-6xl">Read, learn, improve</h1>
-            </div>
-            <div>
-                {BlogContent.map((item) => (
-                    <div key={item.title} className="flex gap-3">
-                        <Image src={item.src} alt={item.alt} width={500} height={500} />
-                        <div>
-                            <h2>{item.title}</h2>
-                            <Badge>{item.badge}</Badge>
+        <div className="min-h-screen w-full items-center justify-center px-3 py-10 text-center md:px-10">
+            <div className="w-[88%] mx-auto flex flex-col h-full justify-center">
+                <div className="pb-10 lg:pb-14">
+                    <Badge>Blog</Badge>
+                    <h1 className="text-left text-6xl font-semibold">Read, learn, improve</h1>
+                </div>
+                <div className="flex flex-wrap h-full w-full items-stretch justify-center gap-5">
+                    {BlogContent.map((item) => (
+                        <div key={item.title} className="flex w-[400px] h-full items-center justify-center flex-col gap-3">
+                            <Image src={item.src} alt={item.alt} width={400} className="h-[300px] w-[400px]" height={400} />
+                            <div>
+                                <Badge>{item.badge}</Badge>
+                                <h2 className="text-xl font-bold">{item.title}</h2>
+                            </div>
                         </div>
-                    </div>
-                ))}
+                    ))}
+                </div>
             </div>
         </div>
     );
