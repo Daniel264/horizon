9b8bb91f72d519c1edd472604e686d6b979d3bc2
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-05 18:05:22 +0100

    fix: change the id numbers of card-text to be arranged in ascending numbers

--- src/utilities/cardText.ts
+++ src/utilities/cardText.ts
@@ -1,24 +1,24 @@
 export const firstCardText = [
     { id: 1, text: "Market Trend Analysis" },
-    { id: 1, text: "Limited Query Mapping" },
-    { id: 1, text: "Standard Dashboards" },
-    { id: 1, text: "Email Notifications" },
-    { id: 1, text: "Entry-Level Support" },
+    { id: 2, text: "Limited Query Mapping" },
+    { id: 3, text: "Standard Dashboards" },
+    { id: 4, text: "Email Notifications" },
+    { id: 5, text: "Entry-Level Support" },
 ];
 export const secondCardText = [
     { id: 1, text: "Everything in Explorer" },
-    { id: 1, text: "Predictive Analytics" },
-    { id: 1, text: "Advanced Query Mapping" },
-    { id: 1, text: "Customizable Dashboards" },
-    { id: 1, text: "Smart Alert System" },
-    { id: 1, text: "Extended Support" },
+    { id: 2, text: "Predictive Analytics" },
+    { id: 3, text: "Advanced Query Mapping" },
+    { id: 4, text: "Customizable Dashboards" },
+    { id: 5, text: "Smart Alert System" },
+    { id: 6, text: "Extended Support" },
 ];
 
 export const thirdCardText = [
     { id: 1, text: "Everything in Innovator" },
-    { id: 1, text: "Competitor Benchmarking" },
-    { id: 1, text: "Holistic Market Visualization" },
-    { id: 1, text: "Adaptive Strategy Planner" },
-    { id: 1, text: "24/7 Priority Support" },
-    { id: 1, text: "Custom Integrations" },
+    { id: 2, text: "Competitor Benchmarking" },
+    { id: 3, text: "Holistic Market Visualization" },
+    { id: 4, text: "Adaptive Strategy Planner" },
+    { id: 5, text: "24/7 Priority Support" },
+    { id: 6, text: "Custom Integrations" },
 ];
