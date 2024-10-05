03047b9d7ecc9a2efc522d55c2cf129f55a82fa0
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-05 19:38:24 +0100

    feat: integrate BlogContent into Blog component for dynamic rendering

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { Badge } from "./Badge";
+import { BlogContent } from "@/utilities/blog";
 
 export const Blog = () => {
     <div className="min-h-screen w-full">
@@ -8,7 +9,15 @@ export const Blog = () => {
             <h1 className="text-6xl">Read, learn, improve</h1>
         </div>
         <div>
-            
+            {BlogContent.map((item) => (
+                <div key={item.title} className="flex gap-3">
+                    <Image src={item.src} alt={item.alt} width={500} height={500} />
+                    <div>
+                        <h2>{item.title}</h2>
+                        <Badge>{item.badge}</Badge>
+                    </div>
+                </div>
+            ))}
         </div>
     </div>;
 };
