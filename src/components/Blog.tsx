47d66b07663b2db2bde5a2c9da81ef7eae8cdb94
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-05 19:58:09 +0100

    feat: enhance Blog component layout with button for navigating to all articles

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,16 +1,20 @@
 import Image from "next/image";
 import { Badge } from "./Badge";
 import { BlogContent } from "@/utilities/blog";
+import { Button } from "./Button";
 
 export const Blog = () => {
     return (
         <div className="min-h-screen w-full items-center justify-center px-3 py-10 text-center md:px-10">
             <div className="w-[88%] mx-auto flex flex-col h-full justify-center">
-                <div className="pb-10 lg:pb-14">
-                    <Badge>Blog</Badge>
-                    <h1 className="text-left text-6xl font-semibold">Read, learn, improve</h1>
+                <div className="pb-10 lg:pb-14 flex justify-between h-full w-full items-center">
+                    <div>
+                        <Badge>Blog</Badge>
+                        <h1 className="text-left text-6xl font-semibold">Read, learn, improve</h1>
+                    </div>
+                    <Button backgroundColor={""} color={""} >All articles</Button>
                 </div>
-                <div className="flex flex-wrap h-full w-full items-stretch justify-center gap-5">
+                <div className="flex flex-col lg:flex-row flex-wrap h-full w-full items-stretch justify-center gap-5">
                     {BlogContent.map((item) => (
                         <div key={item.title} className="flex w-[400px] h-full items-center justify-center flex-col gap-3">
                             <Image src={item.src} alt={item.alt} width={400} className="h-[300px] w-[400px]" height={400} />
