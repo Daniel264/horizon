68501772453ea7fff217586cc6f494a8022baa2c
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-05 17:53:57 +0100

    refactor: space the 3 card elements evenly

--- src/components/PricingCard.tsx
+++ src/components/PricingCard.tsx
@@ -1,11 +1,25 @@
+import { Button } from "./Button";
+
 export const PricingCard = () => {
     return (
-        <div className="w-[380px] h-[500px] flex flex-col justify-center items-center rounded-badge border-2 border-black border-opacity-10 p-5">
-            <p className="text-xl uppercase font-semibold">Explorer</p>
-            <div className="text-4xl font-semibold">
-                $4,99 <span className="text-[#9CA3AF]">/mo</span>
+        <div className="flex h-[450px] w-[380px] flex-col items-center  justify-between rounded-badge border-2 border-black border-opacity-10 p-8">
+            <div className="text-center">
+                <p className=" font-semibold uppercase">Explorer</p>
+                <div className="text-4xl font-bold">
+                    $4,99 <span className="text-[#9CA3AF]">/mo</span>
+                </div>
             </div>
-            <div></div>
+            <div className="w-full">
+                <p className="text-lg font-medium">Lorem, ipsum dolor.</p>
+                <p className="text-lg font-medium">Lorem, ipsum dolor.</p>
+                <p className="text-lg font-medium">Lorem, ipsum dolor.</p>
+                <p className="text-lg font-medium">Lorem, ipsum dolor.</p>
+                <p className="text-lg font-medium">Lorem, ipsum dolor.</p>
+                <p className="text-lg font-medium">Lorem, ipsum dolor.</p>
+            </div>
+            <Button backgroundColor={"bg-cream"} color={"text-black"}>
+                Get notified
+            </Button>
         </div>
     );
 };
