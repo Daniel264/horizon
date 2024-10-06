460862c65ce0e9906877a8602db14b33f530f787
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-06 19:12:37 +0100

    fix: adjust heading sizes and font weights in Pricing and Recommendation components for improved readability

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -12,7 +12,7 @@ export const Pricing = () => {
                 <div>
                     <Badge>Pricing</Badge>
                 </div>
-                <h1 className="text-4xl md:text-6xl font-semibold">
+                <h1 className="text-5xl md:text-6xl font-semibold">
                     Start making <span className="text-[#8B5CF6]">smarter</span> decisions,
                     <p className="text-[#9CA3AF]">Choose a plan</p>
                 </h1>
--- src/components/Recommendation.tsx
+++ src/components/Recommendation.tsx
@@ -7,7 +7,7 @@ export const Recommendation = () => {
         <section className="flex min-h-screen w-full flex-col items-center justify-center space-y-20 bg-[#F3F4F6] py-16">
             <header className="flex h-fit w-full flex-col items-center justify-center">
                 <Badge>Customer Voices</Badge>
-                <h1 className="text-6xl font-semibold ">Early adopters are saying</h1>
+                <h1 className="text-5xl px-5 md:px-0 md:text-6xl font-semibold ">Early adopters are saying</h1>
             </header>
             <div className="grid w-[70%] grid-cols-1 gap-14 lg:grid-cols-2">
                 <div className="space-y-5">
@@ -15,7 +15,7 @@ export const Recommendation = () => {
                         <Image src="/assets/images/recommendone.svg" width={100} height={100} alt="avatar" />
                     </div>
                     <div>
-                        <p className="text-2xl font-light">“We integrated Horizon into our business operations, and the results have been outstanding. The Smart Alert System ensures we&apos;re always informed about critical market developments, giving us a competitive edge.”</p>
+                        <p className="text-2xl font-medium">“We integrated Horizon into our business operations, and the results have been outstanding. The Smart Alert System ensures we&apos;re always informed about critical market developments, giving us a competitive edge.”</p>
                     </div>
                     <div className="space-y-5">
                         <div>
@@ -32,7 +32,7 @@ export const Recommendation = () => {
                         <Image src="/assets/images/recommendtwo.svg" width={100} height={100} alt="" />
                     </div>
                     <div>
-                        <p className="text-2xl font-light">“We integrated Horizon into our business operations, and the results have been outstanding. The Smart Alert System ensures we&apos;re always informed about critical market developments, giving us a competitive edge.”</p>
+                        <p className="text-2xl font-medium">“We integrated Horizon into our business operations, and the results have been outstanding. The Smart Alert System ensures we&apos;re always informed about critical market developments, giving us a competitive edge.”</p>
                     </div>
                     <div className="space-y-5">
                         <div>
@@ -50,7 +50,7 @@ export const Recommendation = () => {
                             Benchmark <span className="text-[#8B5CF6]">against</span> the best companies
                         </h1>
 
-                        <p className="text-xl font-light">Elevate your competitive edge with our Competitor Benchmarking feature. Horizon benchmarks your performance against industry leaders, providing actionable insights to outperform your competition.</p>
+                        <p className="text-xl font-medium">Elevate your competitive edge with our Competitor Benchmarking feature. Horizon benchmarks your performance against industry leaders, providing actionable insights to outperform your competition.</p>
                         <div className="w-fit">
                             <Button backgroundColor={"bg-[#9B72F7]"} color={"text-white"}>
                                 Get early access
