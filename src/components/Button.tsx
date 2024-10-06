dc0e54c7b52432967d9dffb0b580d937e7fbcd89
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-06 23:32:35 +0100

    refactor: simplify Button component styles for improved readability

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -6,10 +6,8 @@ interface ButtonProps {
 
 export const Button = ({ children, backgroundColor, color }: ButtonProps) => {
     return (
-        <button
-            className={`btn-shadow group relative flex h-[45px] w-fit items-center justify-center overflow-hidden rounded-xl px-3 ${backgroundColor} text-sm ${color} font-normal leading-[24px] outline outline-[1px] -outline-offset-[0px] outline-black/[55%] transition-all hover:outline-[3px] hover:-outline-offset-1 hover:outline-purple-400/[100%] md:h-[48px] xl:w-[154px] xl:text-[14px] rounded-xl`}
-        >
-            <div className="absolute -bottom-[2px] -left-1 right-0 z-10 !mx-auto h-[0px] w-[160px] rounded-xl bg-white transition-all duration-300 group-hover:h-[52px]" /> 
+        <button className={`btn-shadow group relative flex h-[45px] w-fit items-center justify-center overflow-hidden rounded-xl px-3 ${backgroundColor} text-sm ${color} rounded-xl font-normal  leading-[24px]   transition-all md:h-[48px] xl:w-[154px] xl:text-[14px]`}>
+            <div className="absolute -bottom-[2px] -left-1 right-0 z-10 !mx-auto h-[0px] w-[160px] rounded-xl bg-white transition-all duration-300 group-hover:h-[52px]" />
             <span className="z-[11] group-hover:text-black">{children}</span>
         </button>
     );
