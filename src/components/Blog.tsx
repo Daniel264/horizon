589c65b3b7e7f2cc519a79a4be9d6c59a06379a1
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-06 19:07:23 +0100

    fix: improve layout and spacing in Blog component for better readability and alignment

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -6,17 +6,19 @@ import { Button } from "./Button";
 export const Blog = () => {
     return (
         <section className="min-h-screen w-full items-center justify-center px-3 py-10 text-center md:px-10">
-            <div className="w-full lg:w-[88%] mx-auto items-center flex flex-col h-full justify-center">
-                <div className="pb-10 lg:pb-14 flex justify-between h-full w-full items-center">
+            <div className="mx-auto flex h-full w-full flex-col items-center justify-center lg:w-[88%]">
+                <div className="flex h-full w-full flex-col items-center justify-between space-y-5 pb-10 md:flex-row lg:pb-14">
                     <div>
                         <Badge>Blog</Badge>
                         <h1 className="text-left text-6xl font-semibold">Read, learn, improve</h1>
                     </div>
-                    <Button backgroundColor={""} color={""} >All articles</Button>
+                    <Button backgroundColor={""} color={""}>
+                        All articles
+                    </Button>
                 </div>
-                <div className="flex flex-col lg:flex-row flex-wrap h-full w-full items-stretch justify-center gap-5">
+                <div className="flex h-full w-full flex-col flex-wrap items-stretch justify-center gap-5 lg:flex-row">
                     {BlogContent.map((item) => (
-                        <div key={item.title} className="flex w-[400px] h-full items-center justify-center mx-auto lg:mx-0 flex-col gap-3">
+                        <div key={item.title} className="mx-auto flex h-full w-[400px] flex-col items-center justify-center gap-3 lg:mx-0">
                             <Image src={item.src} alt={item.alt} width={400} className="h-[300px] w-[400px]" height={400} />
                             <div>
                                 <Badge>{item.badge}</Badge>
