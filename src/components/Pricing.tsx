be49f00d0d3a8cbc330ac664e4652c1bb7bbf2cb
Daniel… <danieloluwafolajimi@gmail.com>
2024-10-06 19:09:06 +0100

    fix: adjust heading size and spacing in Pricing component for improved layout

--- src/components/Pricing.tsx
+++ src/components/Pricing.tsx
@@ -12,7 +12,7 @@ export const Pricing = () => {
                 <div>
                     <Badge>Pricing</Badge>
                 </div>
-                <h1 className="text-6xl font-semibold">
+                <h1 className="text-4xl md:text-6xl font-semibold">
                     Start making <span className="text-[#8B5CF6]">smarter</span> decisions,
                     <p className="text-[#9CA3AF]">Choose a plan</p>
                 </h1>
@@ -25,7 +25,7 @@ export const Pricing = () => {
                 <PricingCard tag={"Innovator"} price={19} buttonText={"text-white"} buttonBackground={"bg-purple-400"} cardText={secondCardText} badge={true} tagColor="text-[#8B5CF6]"/>
                 <PricingCard tag={"Visionary"} price={49} buttonText={"text-black"} buttonBackground={"bg-cream"} cardText={thirdCardText} badge={false} />
             </div>
-            <div className="flex flex-row space-x-7 text-[#8B5CF6]">
+            <div className="flex flex-row space-x-7 px-5 md:px-0 text-[#8B5CF6]">
                 <div className="flex">
                     <Image src={"/assets/images/checkbox.svg"} alt={""} width={23} height={23} />
                     Free Trial
